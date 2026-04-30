9623413a02af4a74e772a61f07ff59c11e155f87
SO-SO <144866516+HITS-SO@users.noreply.github.com>
2026-04-30 16:48:59 +0900

    [level 1] Title: 공원 산책, Time: 0.59 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -21,28 +21,30 @@ class Solution {
             //거리 선언
             int far = Integer.parseInt(order[1]);
             int x = 0, y = 0;
-            
+            //입력 받은 문자에 따라 방향결정
             if(goTO.equals("E")) y = 1;
             else if(goTO.equals("W")) y = -1;
             else if(goTO.equals("S")) x = 1;
             else if(goTO.equals("N")) x = -1;
-            
             int nx = s[0];
             int ny = s[1];
             boolean move = true;
+            //far 만큼 시작값의 좌표 변경
             for(int j = 0; j < far; j++){
                 nx += x;
                 ny += y;
-
+                //이동이 PARK의 범위를 넘어가면 이동 x
                 if(nx < 0 || ny < 0 || nx >= park.length || ny >= park[0].length()){
                     move = false;
                     break;
                 }
+                //장애물 발생시 해당 위치로 이동x
                 if(park[nx].charAt(ny) == 'X'){
                     move = false;
                     break;
                 }
             }
+            //이동에 성공하면 그 값을 반환
             if(move){
                 s[0] = nx;
                 s[1] = ny;
